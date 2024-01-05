2fcfa97f8c92cb6672593e1cf5c0eeb24ce2388a
Your Name <emanuelaraujo90@gmail.com>
2024-01-05 18:35:18 -0300

    criação do repository

--- registra/src/main/java/com/example/registraPessoas/registra/controllers/PessoasController.java
+++ registra/src/main/java/com/example/registraPessoas/registra/controllers/PessoasController.java
@@ -1,7 +1,7 @@
 package com.example.registraPessoas.registra.controllers;
 
 import com.example.registraPessoas.registra.pessoa.Pessoa;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +11,9 @@ import java.util.List;
 @RestController
 @RequestMapping("pessoas")
 public class PessoasController {
+
+    @Autowired
+    PessoaRepository repository;
 
     @GetMapping
     public void getAllPeoples(){
