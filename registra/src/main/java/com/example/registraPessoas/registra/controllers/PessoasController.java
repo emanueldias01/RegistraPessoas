11226dd981357191dc3d8931e4f9f4743cccc00f
Your Name <emanuelaraujo90@gmail.com>
2024-01-05 18:29:10 -0300

    criação do controller principal
    a aplicação vai se basear nesse controller

---
+++ registra/src/main/java/com/example/registraPessoas/registra/controllers/PessoasController.java
@@ -0,0 +1,9 @@
+package com.example.registraPessoas.registra.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pessoas")
+public class PessoasController {
+}
