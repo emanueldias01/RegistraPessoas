8a2cabf101a6de23a907fadddc9cf07de5bc8518
Your Name <emanuelaraujo90@gmail.com>
2024-01-05 18:27:48 -0300

    UPLOAD DOS ARQUIVOS

---
+++ registra/src/main/java/com/example/registraPessoas/registra/RegistraApplication.java
@@ -0,0 +1,13 @@
+package com.example.registraPessoas.registra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistraApplication.class, args);
+	}
+
+}
