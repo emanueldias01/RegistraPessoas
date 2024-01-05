6b55e4f535df01feca9b8259813140bfe81d5be7
Your Name <emanuelaraujo90@gmail.com>
2024-01-05 18:30:46 -0300

    Teste da aplicação
    testando se a aplicação realmente me retornava

--- registra/src/main/java/com/example/registraPessoas/registra/controllers/PessoasController.java
+++ registra/src/main/java/com/example/registraPessoas/registra/controllers/PessoasController.java
@@ -1,9 +1,17 @@
 package com.example.registraPessoas.registra.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("pessoas")
 public class PessoasController {
+
+    @GetMapping
+    public ResponseEntity getAllPeoples(){
+        return ResponseEntity.ok("tudo certo");
+    }
+
 }
