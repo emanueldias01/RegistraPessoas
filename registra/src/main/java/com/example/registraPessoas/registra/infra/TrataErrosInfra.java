66022a03b16977adfd06264338f54d3df85f55d1
Your Name <emanuelaraujo90@gmail.com>
2024-01-15 22:28:28 -0300

    Tratamento do erro 400
    erro 400 = quando os campos não são preenchidos de maneira correta, causa uma exception com validation
    
    NO ATUAL CÓDIGO, O ERRO É TRATADO RETORNANDO A API EM CASO DE ERRO O CAMPO E O ERRO DO CAMPO

--- registra/src/main/java/com/example/registraPessoas/registra/infra/TrataErrosInfra.java
+++ registra/src/main/java/com/example/registraPessoas/registra/infra/TrataErrosInfra.java
@@ -2,6 +2,8 @@ package com.example.registraPessoas.registra.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +16,21 @@ public class TrataErrosInfra {
 
     }
 
-}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+    }
+
+
+
