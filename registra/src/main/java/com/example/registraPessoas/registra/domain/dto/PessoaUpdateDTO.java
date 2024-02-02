8391abc7b250e0e0504dbaf4d8900bbf63ce5a9a
Your Name <emanuelaraujo90@gmail.com>
2024-02-02 17:08:00 -0300

    Autenticação statefull e csrf desabilitadas
    Vou implementar o modelo STATELESS, que por si só já faz a configuração contra csrf VIA TOKEN

--- registra/src/main/java/com/example/registraPessoas/registra/domain/dto/PessoaUpdateDTO.java
+++ registra/src/main/java/com/example/registraPessoas/registra/domain/dto/PessoaUpdateDTO.java
@@ -3,7 +3,6 @@ package com.example.registraPessoas.registra.domain.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record PessoaUpdateDTO(
-        @NotNull
         Long id,
         String email,
         String telefone) {
