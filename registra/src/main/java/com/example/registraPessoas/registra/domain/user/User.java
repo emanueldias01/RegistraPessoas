34fdc113632eda57e6369b86a29d3100a705c5fc
Your Name <emanuelaraujo90@gmail.com>
2024-02-03 11:44:50 -0300

    tornando o "role" de qualquer user para padrão

--- registra/src/main/java/com/example/registraPessoas/registra/domain/user/User.java
+++ registra/src/main/java/com/example/registraPessoas/registra/domain/user/User.java
@@ -5,9 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name= "usuarios")
@@ -23,12 +25,12 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return login;
     }
 
     @Override
