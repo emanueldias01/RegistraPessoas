3711e18f933745e84d4c1194ba3a4ae98d42b017
Your Name <emanuelaraujo90@gmail.com>
2024-02-03 11:55:21 -0300

    refatoração do código

--- registra/src/main/java/com/example/registraPessoas/registra/infra/SecurityConfigurations.java
+++ registra/src/main/java/com/example/registraPessoas/registra/infra/SecurityConfigurations.java
@@ -17,8 +17,10 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.csrf().
-                disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        return http.csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
 
     }
